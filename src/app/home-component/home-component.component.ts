6897cbab16009c666536800f25633c8ea309acf7
Krishna Dihora <kjdih2@gmail.com>
2024-11-02 15:54:34 -0400

    Working search functionality

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,31 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { CommonModule } from '@angular/common';
 import { InventoryServiceService } from '../services/inventory-service.service';
-
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-home-component',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './home-component.component.html',
     styleUrl: './home-component.component.scss',
 })
 export class HomeComponentComponent implements OnInit {
+    searchText: string = '';
+    allParts: any = [];
+    searchedParts: any = [];
+    destroyed = new Subject<void>();
+    screenSize = 'lg';
 
-	searchText: string = '';
-	allParts: any = [];
-	searchedParts: any = [];
-
-    constructor(private inventoryService: InventoryServiceService) {}
+    constructor(
+        private inventoryService: InventoryServiceService,
+        private breakpointObserver: BreakpointObserver
+    ) {}
 
     ngOnInit(): void {
-		this.inventoryService.getAllSearchableParts().subscribe((data: any) => {
-			this.allParts = JSON.parse(JSON.stringify(data));
-			this.searchedParts = JSON.parse(JSON.stringify(data));
-		});
-	}
+        this.manageScreenSize();
 
-	filterItems(event: any) {
-		console.log(event.target.value);
-	}
+        this.inventoryService
+            .getAllSearchableParts()
+            .subscribe((data: any) => {
+                this.allParts = JSON.parse(JSON.stringify(data));
+                this.searchedParts = JSON.parse(JSON.stringify(data));
+            })
+            .add(() => {
+                console.log(this.allParts);
+            });
+    }
+
+    manageScreenSize() {
+        this.breakpointObserver
+            .observe([
+                Breakpoints.XLarge,
+                Breakpoints.Large,
+                Breakpoints.Medium,
+                Breakpoints.Small,
+                '(max-width: 550px)',
+                Breakpoints.XSmall,
+            ])
+            .subscribe((breakpointState: BreakpointState) => {
+                if (breakpointState.breakpoints['(max-width: 550px)']) {
+                    this.screenSize = 'xs';
+                } else if (breakpointState.breakpoints[Breakpoints.XLarge]) {
+                    this.screenSize = 'md';
+                } else if (breakpointState.breakpoints[Breakpoints.Large]) {
+                    this.screenSize = 'md';
+                } else if (breakpointState.breakpoints[Breakpoints.Medium]) {
+                    this.screenSize = 'md';
+                } else if (breakpointState.breakpoints[Breakpoints.Small]) {
+                    this.screenSize = 'sm';
+                } else if (breakpointState.breakpoints[Breakpoints.XSmall]) {
+                    this.screenSize = 'xs';
+                }
+            });
+    }
+
+    filterItems(event: any) {
+        console.log(event.target.value);
+		this.searchedParts = this.allParts.filter((part: any) => {
+			let isPartValid = false;
+			if (part.partName.toLowerCase().includes(event.target.value.toLowerCase())) {
+				isPartValid = true;
+			}
+			if (part.sku.toLowerCase().includes(event.target.value.toLowerCase())) {
+				isPartValid = true;
+			}
+			if (part.category.toLowerCase().includes(event.target.value.toLowerCase())) {
+				isPartValid = true;
+			}
+			if (part.description.toLowerCase().includes(event.target.value.toLowerCase())) {
+				isPartValid = true;
+			}
+			if (part.locations.some((location: any) => location.binCode.toLowerCase().includes(event.target.value.toLowerCase()))) {
+				isPartValid = true;
+			}
+			return isPartValid;
+		});
+    }
 }
--- src/app/services/inventory-service.service.ts
+++ src/app/services/inventory-service.service.ts
@@ -7,52 +7,248 @@ import { Observable, Observer } from 'rxjs';
 export class InventoryServiceService {
     constructor() {}
 
-	getAllSearchableParts(): Observable<any> {
-
-
+	getAllSearchableParts() {
 		
 		const inventoryParts = [
 			{
-				partName: '',
-				sku: '',
-				category: '',
-				description: '',
+				partName: 'Screws',
+				sku: '11111111',
+				category: 'Metal',
+				description: 'screws',
 				quantity: {
 					totalQuantity: 0,
 					availableQuantity: 0,
 				},
 				locations: [
 					{
-						binCode: '',
-						binDescription: '',
+						binCode: 'a',
+						binDescription: 'a',
 						binQuantity: 0,
 					},
 					{
-						binCode: '',
-						binDescription: '',
+						binCode: 'a',
+						binDescription: 'a',
 						binQuantity: 0,
 					},
 				],
 				movementHistory: [
 					{
-						movementDate: '',
-						fromBin: '',
-						toBin: '',
+						movementDate: 'a',
+						fromBin: 'a',
+						toBin: 'a',
 						quantityMoved: 0,
-						remarks: '',
+						remarks: 'a',
+					}
+				]
+			},
+			{
+				partName: 'Nails',
+				sku: '11111111',
+				category: 'Metal',
+				description: 'screws',
+				quantity: {
+					totalQuantity: 0,
+					availableQuantity: 0,
+				},
+				locations: [
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+				],
+				movementHistory: [
+					{
+						movementDate: 'a',
+						fromBin: 'a',
+						toBin: 'a',
+						quantityMoved: 0,
+						remarks: 'a',
+					}
+				]
+			},
+			{
+				partName: 'Screwdrivers',
+				sku: '11111111',
+				category: 'Metal',
+				description: 'screws',
+				quantity: {
+					totalQuantity: 0,
+					availableQuantity: 0,
+				},
+				locations: [
+					{
+						binCode: 'a',
+						binDescription: 'adsfadfsaasdfsdaffsda',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'asfdasfdsdfafsdaf',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'asfdasfdsdfafsdaf',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'asfdasfdsdfafsdaf',
+						binQuantity: 0,
+					},
+				],
+				movementHistory: [
+					{
+						movementDate: 'a',
+						fromBin: 'a',
+						toBin: 'a',
+						quantityMoved: 0,
+						remarks: 'a',
+					}
+				]
+			},
+			{
+				partName: 'Pencils',
+				sku: '11111111',
+				category: 'Metal',
+				description: 'screws',
+				quantity: {
+					totalQuantity: 0,
+					availableQuantity: 0,
+				},
+				locations: [
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+				],
+				movementHistory: [
+					{
+						movementDate: 'a',
+						fromBin: 'a',
+						toBin: 'a',
+						quantityMoved: 0,
+						remarks: 'a',
+					}
+				]
+			},
+			{
+				partName: 'Root Beer',
+				sku: '11111111',
+				category: 'Metal',
+				description: 'screws',
+				quantity: {
+					totalQuantity: 0,
+					availableQuantity: 0,
+				},
+				locations: [
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+				],
+				movementHistory: [
+					{
+						movementDate: 'a',
+						fromBin: 'a',
+						toBin: 'a',
+						quantityMoved: 0,
+						remarks: 'a',
+					}
+				]
+			},
+			{
+				partName: 'Autons',
+				sku: '11111111',
+				category: 'Software',
+				description: 'ryan deleted the autons',
+				quantity: {
+					totalQuantity: 0,
+					availableQuantity: 0,
+				},
+				locations: [
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+					{
+						binCode: 'a',
+						binDescription: 'a',
+						binQuantity: 0,
+					},
+				],
+				movementHistory: [
+					{
+						movementDate: 'a',
+						fromBin: 'a',
+						toBin: 'a',
+						quantityMoved: 0,
+						remarks: 'a',
 					}
 				]
 			},
 		]
 
-
-
-
-		// return data as an observable
-		const observable = new Observable((observer: Observer<any>) => {
+		return new Observable((observer: Observer<any>) => {
 			observer.next(inventoryParts);
 			observer.complete();
 		});
-		return observable;
+
 	}
 }
+
+
+/*
+const inventoryParts = [
+	{
+		partName: '',
+		sku: '',
+		category: '',
+		description: '',
+		quantity: {
+			totalQuantity: 0,
+			availableQuantity: 0,
+		},
+		locations: [
+			{
+				binCode: '',
+				binDescription: '',
+				binQuantity: 0,
+			},
+			{
+				binCode: '',
+				binDescription: '',
+				binQuantity: 0,
+			},
+		],
+		movementHistory: [
+			{
+				movementDate: '',
+				fromBin: '',
+				toBin: '',
+				quantityMoved: 0,
+				remarks: '',
+			}
+		]
+	},
+]
+*/
